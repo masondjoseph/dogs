77592cd6df2dde6d3a875186ffef1fde8922f783
Mason Joseph <mdjoseph@email.sc.edu>
2021-05-20 12:57:19 -0400

    name

--- Dogs.java
+++ Dogs.java
@@ -2,6 +2,9 @@ package dogs;
 
 public class Dogs {
 
+    private String name;
+    private String owner;
+
     public static void main(String[] args) {
         System.out.print("Hello world");
     }
