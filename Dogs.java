69528ba5ef2ae2b7cd2ac7f1eae1c2fd38dbe05b
portiaportia <portiaplante@gmail.com>
2021-05-20 12:57:53 -0400

    adding stuff to dog

--- Dogs.java
+++ Dogs.java
@@ -2,6 +2,18 @@ package dogs;
 
 public class Dogs {
 
+    public class Dogs() {
+        System.out.println("Creating a Dog");
+    }
+
+    public void woof(){
+        System.out.println("woof woof woof");
+    }
+
+    public void eat(){
+        System.out.println("Eating puppy chow");
+    }
+
     public static void main(String[] args) {
         System.out.print("Hello world");
     }
