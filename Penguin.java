f33a42df99e2460c5f8f25426480e2c98d6fe976
portiaportia <portiaplante@gmail.com>
2021-05-20 12:48:24 -0400

    changes

---
+++ Penguin.java
@@ -0,0 +1,11 @@
+public class Penguin{
+    private String title;
+
+    public Penguin(String title){
+        this.title = title;
+    }
+
+    public String toString(){
+        return title + " is a penguin";
+    }
+}
