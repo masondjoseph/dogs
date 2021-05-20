9db149a712d516b19c308b91f7a1ddb8ebe8d2a4
Rahul Bulusu <rbbulusu@gmail.com>
2021-05-20 12:58:24 -0400

    changes

--- Dogs.java
+++ Dogs.java
@@ -1,8 +1,10 @@
-package dogs;
-
 public class Dogs {
 
     public static void main(String[] args) {
         System.out.print("Hello world");
+        System.out.println("This is an interesting class");
     }
+    private String title;
+
+
 }
