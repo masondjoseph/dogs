599da0f253832d5a3250fd8bce1513d9f80e379d
Rahul Bulusu <rbbulusu@gmail.com>
2021-05-20 12:48:43 -0400

    changes

---
+++ cat.java
@@ -0,0 +1,9 @@
+public class cat{
+    private String title;
+    public Cat (String title){
+        this.title = title;
+    }
+    public String toString(){
+        return title + " is a cat";
+    }
+}
