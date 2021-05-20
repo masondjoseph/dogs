231197e2e8769f5297938015dfda01278d03fa92
Mason Joseph <mdjoseph@email.sc.edu>
2021-05-20 13:10:59 -0400

    Mason.java

---
+++ Mason.java
@@ -0,0 +1,10 @@
+public class Mason {
+    
+    private String first;
+    private String last;
+
+    public Mason(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+}
